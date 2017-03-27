1e7f7e2fba3f461ebe8254a1f10db38378ed49b5
Dr. Martin Lützelberger <m.luetzelberger@telecolumbus.net>
2017-03-27 19:12:37 +0200

    Insert delay(50); before sensor.init()

--- VL6180x.ino
+++ VL6180x.ino
@@ -28,10 +28,12 @@ hard-coded address of the module.
 #include <Wire.h>
 #include <VL6180X.h>
 
+#define RANGE 1
+
 /* List of adresses for each sensor - after reset the address can be configured */
 #define address0 0x20
 #define address1 0x22
-//#define address2 0x24
+// #define address2 0x24
 // #define address1 0x26
 // #define address1 0x28
 // #define address1 0x30
@@ -82,6 +84,7 @@ void setup()
   // Sensor0
   Serial.println("Start Sensor 0");
   digitalWrite(enablePin0, HIGH);
+  delay(50);
   sensor0.init();
   sensor0.configureDefault();
   sensor0.setAddress(address0);
@@ -90,14 +93,15 @@ void setup()
   sensor0.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
   sensor0.setTimeout(500);
   sensor0.stopContinuous();
-  sensor0.setScaling(3); // configure range or precision 1, 2 oder 3 mm
+  sensor0.setScaling(RANGE); // configure range or precision 1, 2 oder 3 mm
   delay(300);
   sensor0.startInterleavedContinuous(100);
-  delay(1000);
+  delay(100);
   
   // Sensor1
   Serial.println("Start Sensor 1");
   digitalWrite(enablePin1, HIGH);
+  delay(50);
   sensor1.init();
   sensor1.configureDefault();
   sensor1.setAddress(address1);
@@ -106,43 +110,45 @@ void setup()
   sensor1.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
   sensor1.setTimeout(500);
   sensor1.stopContinuous();
-  sensor1.setScaling(3);
+  sensor1.setScaling(RANGE);
   delay(300);
   sensor1.startInterleavedContinuous(100);
+  delay(100);
 
-  // Sensor2
-  //  Serial.println("Start Sensor 2");
-  //  digitalWrite(enablePin1, HIGH);
-  //  sensor2.init();
-  //  sensor2.configureDefault();
-  //  sensor2.setAddress(address1);
-  //  Serial.println(sensor1.readReg(0x212),HEX);
-  //  sensor2.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
-  //  sensor2.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
-  //  sensor2.setTimeout(500);
-  //  sensor2.stopContinuous();
-  //  sensor2.setScaling(3);
-  //  delay(300);
-  //  sensor2.startInterleavedContinuous(100);
+//  Sensor2
+//  Serial.println("Start Sensor 2");
+//  digitalWrite(enablePin2, HIGH);
+//  delay(50);
+//  sensor2.init();
+//  sensor2.configureDefault();
+//  sensor2.setAddress(address2);
+//  Serial.println(sensor2.readReg(0x212),HEX);
+//  sensor2.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
+//  sensor2.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+//  sensor2.setTimeout(500);
+//  sensor2.stopContinuous();
+//  sensor2.setScaling(RANGE);
+//  delay(300);
+//  sensor2.startInterleavedContinuous(100);
   
   // copy & paste as much sensors as you require
   
   delay(1000);
  
-  Serial.println("Sensors ready! Start reading sensors in 5 seconds ...!");
-  delay(5000);
+  Serial.println("Sensors ready! Start reading sensors in 3 seconds ...!");
+  delay(3000);
 }
 
 void loop()
 {
   // Read daylight (LUX)
-  Serial.print("\tDaylight0: ");
-  Serial.print(sensor0.readAmbientContinuous());
-  if (sensor0.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  //Serial.print("\tDaylight0: ");
+  //Serial.print(sensor0.readAmbientContinuous());
+  //if (sensor0.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
-  Serial.print("\tDaylight1: ");
-  Serial.print(sensor1.readAmbientContinuous());
-  if (sensor1.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  //Serial.print("\tDaylight1: ");
+  //Serial.print(sensor1.readAmbientContinuous());
+  //if (sensor1.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
   //Serial.print("\tDaylight2: ");
   //Serial.print(sensor1.readAmbientContinuous());
@@ -159,13 +165,13 @@ void loop()
   Serial.print(sensor1.readRangeContinuousMillimeters());
   if (sensor1.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
-  //Serial.print("\tDistance2: ");
-  //Serial.print(sensor1.readRangeContinuousMillimeters());
-  //if (sensor2.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+//  Serial.print("\tDistance2: ");
+//  Serial.print(sensor2.readRangeContinuousMillimeters());
+//  if (sensor2.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
 // copy & paste as much sensors as you require
 
   Serial.println();
   
-  delay(300);
+  delay(100);
 }
